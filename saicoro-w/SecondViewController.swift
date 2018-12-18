2555c2597c0a57b675a62a0fcd2529aca2fe08f7
akkarin <at.mattenn@gmail.com>
2018-12-18 18:32:11 +0900

    first commit

---
+++ saicoro-w/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  saicoro-w
+//
+//  Created by 友利 奈緒 on 2018/12/18.
+//  Copyright © 2018年 MATTENN. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
