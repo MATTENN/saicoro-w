5642119aab3aff7217006f9941634d18b0a89d80
akkarin <at.mattenn@gmail.com>
2018-12-18 17:07:39 +0900

    Initial Commit

---
+++ saicoro-w/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  saicoro-w
+//
+//  Created by 友利 奈緒 on 2018/12/18.
+//  Copyright © 2018年 MATTENN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ saicoro-wTests/saicoro_wTests.swift
@@ -0,0 +1,34 @@
+//
+//  saicoro_wTests.swift
+//  saicoro-wTests
+//
+//  Created by 友利 奈緒 on 2018/12/18.
+//  Copyright © 2018年 MATTENN. All rights reserved.
+//
+
+import XCTest
+@testable import saicoro_w
+
+class saicoro_wTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
