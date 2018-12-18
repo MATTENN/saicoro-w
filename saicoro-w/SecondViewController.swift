97d3002072c3238f608b5b9b34b1dc1ccefadae7
akkarin <at.mattenn@gmail.com>
2018-12-18 18:36:30 +0900

    add overwriteLabel

--- saicoro-w/SecondViewController.swift
+++ saicoro-w/SecondViewController.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.field1.delegate = self
     }
     
-
+    @IBOutlet weak var field1: UITextField!
+    @IBOutlet weak var label1: UILabel!
+    
     /*
     // MARK: - Navigation
 
@@ -26,5 +29,30 @@ class SecondViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // キーボードを閉じる
+        field1.resignFirstResponder()
+        return true
+    }
+    
+    let First = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "ViewController") as! ViewController
+    
+    @IBAction func reflect(_ sender: Any) {
+        label1.text = field1.text!
+        First.overwriteLabel(text: field1.text ?? "none")
+        
+    }
+    
+    @IBAction func reflectsecond(_ sender: Any) {
+        label1.text = field1.text!
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        field1.resignFirstResponder()
+    }
+    
+    
 
 }
--- saicoro-w/ViewController.swift
+++ saicoro-w/ViewController.swift
@@ -14,7 +14,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBAction func unwindToTop(segue: UIStoryboardSegue){
+        //Second View Controllerから元に戻れるやつ
+    }
+    
+    func overwriteLabel(text: String){
+        label1.text = text
+    }
 }
 
